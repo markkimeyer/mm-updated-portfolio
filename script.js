ce0d39f025815a08618c73c37acbcce4b412aea3
Markki Meyer <markkimeyer@gmail.com>
2021-02-08 19:09:08 -0600

    copy of portfolio plus gallery revisions

---
+++ script.js
@@ -0,0 +1,39 @@
+$(document).ready( function() {
+
+
+    
+
+
+
+
+//On click projectone live page
+
+$(document).on("click", "#project-one-live", function () {
+    window.open("https://markkimeyer.github.io/RechargeCarApp/");
+})
+
+//on hover
+// $(document).on("hover", "#project-one-live", function () {
+//     $text(Live Github Page);
+// })
+
+//On click projectone repo 
+
+$(document).on("click", "#project-one-repo", function () {
+    window.open("https://github.com/markkimeyer/RechargeCarApp");
+})
+
+//On click projecttwolive page
+
+$(document).on("click", "#project-two-live", function () {
+    window.open("https://markkimeyer.github.io/daily-planner/");
+})
+
+//On click projecttwo repo 
+
+$(document).on("click", "#project-two-repo", function () {
+    window.open("https://github.com/markkimeyer/daily-planner");
+})
+
+//don't delete closing tab
+});
