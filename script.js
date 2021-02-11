2e11212901dcd37922118c73688b6acf4993656b
Markki Meyer <markkimeyer@gmail.com>
2021-02-10 21:13:33 -0600

    links on contact page

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ $(document).ready( function() {
 
 
 
+
+
 //On click projectone live page
 
 $(document).on("click", "#project-one-live", function () {
@@ -34,6 +36,25 @@ $(document).on("click", "#project-two-live", function () {
 $(document).on("click", "#project-two-repo", function () {
     window.open("https://github.com/markkimeyer/daily-planner");
 })
+//On click project 3 live page
+$(document).on("click", "#project-three-live", function () {
+    window.open("https://markkimeyer.github.io/codeQUIZ/");
+})
 
+//On click project3 repo 
+
+$(document).on("click", "#project-three-repo", function () {
+    window.open("https://github.com/markkimeyer/codeQUIZ");
+})
+
+//email pop up form
+$(document).on("click", "#mailchimp", function () {
+    window.open("https://us1.list-manage.com/contact-form?u=ab348f2b61785522dbd6bdd88&form_id=73c9c3b608e09594c381e853f8e5ce8d");
+})
+
+//resume link to google doc  "resume"
+$(document).on("click", "#resume", function () {
+    window.open("https://drive.google.com/file/d/1V8nwHXaHcTvJLf5I8pivfCqoQdf-blCp/view?usp=sharing");
+})
 //don't delete closing tab
-});
+})
